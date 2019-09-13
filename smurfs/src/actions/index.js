33b84059147951dbdec2d4c3d80841bb1c066913
Jenn Soderborg <jsoderborg7@gmail.com>
2019-09-13 11:29:19 -0600

    added addSmurf action, trying to get form data to render automatically

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -3,8 +3,10 @@ import axios from 'axios';
 export const FETCHING_SMURFS_START = "FETCHING_SMURF_START";
 export const FETCHING_SMURFS_SUCCESS = "FETCHING_SMURF_SUCCESS";
 export const FETCHING_SMURFS_FAILURE = "FETCHING_SMURF_FAILURE";
-export const ADD_SMURF = "ADD_SMURF";
-export const UPDATE_VILLAGE = "UPDATE_VILLAGE";
+export const ADD_SMURF_START = "ADD_SMURF_START";
+export const ADD_SMURF_SUCCESS = "ADD_SMURF_SUCCESS";
+export const ADD_SMURF_FAILURE = "ADD_SMURF_FAILURE";
+
 
 export const getSmurfs = () => dispatch =>{
   dispatch({type: FETCHING_SMURFS_START});
@@ -16,5 +18,18 @@ export const getSmurfs = () => dispatch =>{
     })
     .catch(err =>{
       dispatch({type: FETCHING_SMURFS_FAILURE, payload: `${err.response}`});
+    });
+};
+
+export const addSmurf = () dispatch =>{
+  dispatch({type: ADD_SMURF_START});
+  axios
+    .post('http://localhost:3333/smurfs')
+    .then(res =>{
+      console.log('add res', res);
+      dispatch({type: ADD_SMURF_SUCCESS, payload: res.data});
+    })
+    .catch(err =>{
+      ({type: ADD_SMURF_FAILURE, payload: `${err.response}`})
     });
 };
